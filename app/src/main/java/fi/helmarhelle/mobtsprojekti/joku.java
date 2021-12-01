e9c33ba1222ec204e8b3a64a13852a3c945f4a05
helmarhelle <helmarh@metropolia.fi>
2021-12-01 12:17:36 +0200

    alku sekoilua

---
+++ app/src/main/java/fi/helmarhelle/mobtsprojekti/joku.java
@@ -0,0 +1,10 @@
+package fi.helmarhelle.mobtsprojekti;
+
+/**
+ * joku class
+ * @author helkk
+ * @version 0.1
+ */
+public class joku {
+
+}
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"fi.helmarhelle.mobtsprojekti","l":"ExampleInstrumentedTest"},{"p":"fi.helmarhelle.mobtsprojekti","l":"ExampleUnitTest"},{"p":"fi.helmarhelle.mobtsprojekti","l":"joku"},{"p":"fi.helmarhelle.mobtsprojekti","l":"MainActivity"}]

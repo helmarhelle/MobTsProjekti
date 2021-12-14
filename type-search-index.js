0ca004f2aab47defb46670665b6e05e4c64d9708
helmarhelle <helmarh@metropolia.fi>
2021-12-14 10:26:50 +0200

    poistettu turhia tiedostoja

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"fi.helmarhelle.mobtsprojekti","l":"ExampleInstrumentedTest"},{"p":"fi.helmarhelle.mobtsprojekti","l":"ExampleUnitTest"},{"p":"fi.helmarhelle.mobtsprojekti","l":"joku"},{"p":"fi.helmarhelle.mobtsprojekti","l":"MainActivity"}]

8e985278504060ca5261ee47f81a99771dbc6641
helmarhelle <helmarh@metropolia.fi>
2021-12-15 15:21:48 +0200

    KuukaudenEdistyminen aktiviteetin muuntelua

--- app/src/main/java/fi/helmarhelle/mobtsprojekti/KuukaudenEdistyminen.java
+++ app/src/main/java/fi/helmarhelle/mobtsprojekti/KuukaudenEdistyminen.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class KuukaudenEdistyminen extends AppCompatActivity {
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
+public class KuukaudenEdistyminen extends AppCompatActivity {
+        float Unimuuttuja;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kuukauden_edistyminen);
+        
+        GraphView graph = (GraphView) findViewById(R.id.graphkkUni);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0,0),
+                new DataPoint(1, Unimuuttuja/7),
+                new DataPoint(2, Unimuuttuja*2/7),
+                new DataPoint(3, Unimuuttuja*3/7),
+                new DataPoint(4, Unimuuttuja*4/7),
+                new DataPoint(5,Unimuuttuja*5/7),
+                new DataPoint(6, Unimuuttuja*6/7),
+                new DataPoint(7, Unimuuttuja),
+
+        });
+        graph.addSeries(series);
     }
 }
